c97f9819c73be79f69f0b35361e2334cbf889a4e
Cristian Rojas <cristianguillermorojas@gmail.com>
2015-04-21 16:11:55 -0500

    Header styles

--- scentrade/urls.py
+++ scentrade/urls.py
@@ -1,10 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'scentrade.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
     url(r'^admin/', include(admin.site.urls)),
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
