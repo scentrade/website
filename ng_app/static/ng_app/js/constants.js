4e32f018f17f1f12de6a32b3b20e0b80e3b96f55
Cristian Rojas <cristianguillermorojas@gmail.com>
2015-05-26 09:51:01 -0500

    fixed address for api

--- ng_app/static/ng_app/js/constants.js
+++ ng_app/static/ng_app/js/constants.js
@@ -3,5 +3,5 @@
 
   angular.module('scentrade')
     .constant('API_URL_DEV', 'http://scentrade:8003/[language]/api/')
-    .constant('API_URL_PROD', 'http://45.55.203.35:8000/[language]/api/');
+    .constant('API_URL_PROD', 'http://scentrade.cristianrojas.com/[language]/api/');
 })();
