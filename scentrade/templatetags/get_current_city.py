e1801bb24460e4f06735524009fcc72a433b5bde
Cristian Rojas <cristianguillermorojas@gmail.com>
2015-06-30 07:36:40 -0500

    fix

--- scentrade/templatetags/get_current_city.py
+++ scentrade/templatetags/get_current_city.py
@@ -5,10 +5,19 @@ from django.contrib.gis.geoip import GeoIP
 register = template.Library()
 
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
 @register.simple_tag(takes_context=True)
 def get_current_city(context):
     request = context['request']
-    ip = request.META.get('REMOTE_ADDR', None)
+    ip = get_client_ip(request)
     print ip
     g = GeoIP()
     country = g.country(ip)
