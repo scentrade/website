1a2aab63cedede1cac7fa8105d40e12f5e87fc1e
Cristian Rojas <cristianguillermorojas@gmail.com>
2015-06-30 07:50:54 -0500

    current city as context_processor

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,4 +1,25 @@
 from django.conf import settings
+from django.contrib.gis.geoip import GeoIP
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+def get_current_city(request):
+    ip = get_client_ip(request)
+    g = GeoIP()
+    country = g.country(ip)
+    country_code = country['country_code']
+    if country_code == 'MX':
+        return country_code
+    # By default show content for Colombia
+    return 'CO'
 
 
 def global_variables(request):
@@ -7,6 +28,6 @@ def global_variables(request):
     """
     data = {
         'DEBUG': settings.DEBUG,
-        'PERRITO': 'PERRITO'
+        'CURRENT_CITY': get_current_city(request)
     }
     return data
